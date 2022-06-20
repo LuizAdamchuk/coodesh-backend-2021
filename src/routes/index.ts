ff717f61cbaf0e8af29f915822141e516a378550
Luiz Adamchuk Jr <adamchuk.junior@gmail.com>
2022-06-20 15:45:36 -0300

    welcome page json

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { WelcomeController } from "../app/cases/welcome/welcome";
 import articlesRouter from "./articles.routes";
 
 const router = Router();
-
+router.get("/", new WelcomeController().handle);
 router.use("/articles", articlesRouter);
 
 export default router;
